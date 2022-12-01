75da43075f6ff684fc4280b36cb9749af60bd695
Miguel Ferrer Fornali <mferrerf@ext.vectoritcgroup.com>
2022-12-01 13:07:15 +0100

    NEW: Add MVVM View template

---
+++ Template/MVVM View/MVVM View.xctemplate/___VARIABLE_viewName___View.swift
@@ -0,0 +1,43 @@
+//
+//  ___FILEBASENAME___.swift
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//
+
+import UIKit
+import Combine
+
+enum ___FILEBASENAME___State {
+    // Define states
+}
+
+final class ___FILEBASENAME___: XibView {
+    private var subscriptions = Set<AnyCancellable>()
+    private var subject = PassthroughSubject<___FILEBASENAME___State, Never>()
+    public var publisher: AnyPublisher<___FILEBASENAME___State, Never> {
+        subject.eraseToAnyPublisher()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+        bind()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupView()
+        bind()
+    }
+}
+
+private extension ___FILEBASENAME___ {
+    func bind() {
+        // Bind states
+    }
+    
+    func setupView() {
+        // Configure view
+    }
+}
